b56a9a2afc84fb5775e32d20e657ea8941d9a7d6
osparking <jbpark03@gmail.com>
2024-02-10 14:38:42 +0900

    각 시험전 설정 메소드 - 컴파일 오류

--- src/test/java/space/bum/junit/tdd/AirportTest.java
+++ src/test/java/space/bum/junit/tdd/AirportTest.java
@@ -1,9 +1,11 @@
 package space.bum.junit.tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 
 import space.bum.junit.tdd.model.Flight;
+import space.bum.junit.tdd.model.Passenger;
 
 class AirportTest {
 
@@ -12,7 +14,14 @@ class AirportTest {
   class EconomyFlightTest {
     private Flight economyFlight;
     private Passenger min;
-    private Passenger jung;    
+    private Passenger jung;
+
+    @BeforeEach
+    void setUp() {
+      economyFlight = new EconomyFlight("1");
+      min = new Passenger("민영철", false);
+      jung = new Passenger("정성국", true);
+    }
   }
 
 }
