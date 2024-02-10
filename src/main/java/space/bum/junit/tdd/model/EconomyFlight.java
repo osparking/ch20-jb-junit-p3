318e34d2aaad9c0bbf534aab8810b1813730c20e
osparking <jbpark03@gmail.com>
2024-02-10 16:10:57 +0900

    일반 항공편 구현

---
+++ src/main/java/space/bum/junit/tdd/model/EconomyFlight.java
@@ -0,0 +1,23 @@
+package space.bum.junit.tdd.model;
+
+public class EconomyFlight extends Flight {
+
+  public EconomyFlight(String id) {
+    super(id);
+  }
+
+  @Override
+  public boolean addPassenger(Passenger passenger) {
+    return passengers.add(passenger);
+  }
+
+  @Override
+  public boolean removePassenger(Passenger passenger) {
+    if (passenger.isVip()) {
+      return false;
+    } else {
+      return passengers.remove(passenger);
+    }
+  }
+
+}
